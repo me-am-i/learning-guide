389b2350ba7c1b33cf5f319dbaf87a8ede747788
tangweikun <819573105@qq.com>
2016-11-27 21:00:38 +0800

    add loader

--- webpack-demo/demo1/entry.js
+++ webpack-demo/demo1/entry.js
@@ -2,3 +2,9 @@ document.write('It works.')
 
 // 添加模块
 document.write(require('./module.js'))
+
+require('!style!css!./style.css')
+
+// require('./style.css')
+// 若不写 loader 前缀, webpack 编译时需使用:
+// webpack entry.js bundle.js --module-bind 'css=style!css'
