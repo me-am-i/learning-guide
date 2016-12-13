e435cfcab8dd503257b3edda252f8a4949256c28
tangweikun <819573105@qq.com>
2016-12-13 19:56:52 +0800

    :sparkles: feat: creating database and connecting

--- node-api/server.js
+++ node-api/server.js
@@ -1,11 +1,15 @@
 var express = require('express')
 var app = express()
 var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
 var port = process.env.PORT || 8080
+
+mongoose.connect('mongodb://127.0.0.1/test')
+var Bear = require('./app/models/bear')
 
 var router = express.Router()
 
