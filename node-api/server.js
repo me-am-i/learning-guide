1f35573df2a21bb3962060f9050108903173fddf
tangweikun <819573105@qq.com>
2016-12-13 20:52:33 +0800

    :sparkles: feat: updating a bear’s info

--- node-api/server.js
+++ node-api/server.js
@@ -46,6 +46,16 @@ router.route('/bears/:bear_id')
       res.json(bear)
     })
   })
+  .put(function(req, res) {
+    Bear.findById(req.params.bear_id, function(err, bear) {
+      if (err) res.send(err)
+      bear.name = req.body.name || 'twk'
+      bear.save(function(err) {
+        if (err) res.send(err)
+        res.json({ message: 'Bear updated!' })
+      })
+    })
+  })
 
 app.use('/api', router)
 
