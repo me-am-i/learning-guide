f9e3d81cdee0857be40141624880e8b43940421b
tangweikun <819573105@qq.com>
2016-12-15 22:10:09 +0800

    :sparkles:  hooking up Firebase
    create a project in firebase’s website, then paste the url to main.js

--- vue/gkeep-vueifire/src/main.js
+++ vue/gkeep-vueifire/src/main.js
@@ -2,6 +2,19 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import Firebase from 'firebase'
+
+let firebase = new Firebase('https://gkeep-vueifire-6d2db.firebaseio.com')
+
+firebase.child('notes').set([
+  {title: 'Hello world', content: 'Lorem ipsum'}
+])
+
+firebase.child('notes').on('value', (snapshot) => {
+  let notes = snapshot.val()
+  console.log(notes)
+  window.alert(notes[0].title)
+})
 
 /* eslint-disable no-new */
 new Vue({
