ef3e80e9d0cccd1989ed1d35d1c050479f24e964
tangweikun <819573105@qq.com>
2017-01-15 17:49:44 +0800

    :sparkles: struct

---
+++ c/struct3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Books
+{
+  char title[50];
+  char author[50];
+  char subject[100];
+  int book_id;
+};
+
+void printBook(struct Books *book);
+int main()
+{
+  struct Books Book1;
+  struct Books Book2;
+
+  strcpy(Book1.title, "C Programming");
+  strcpy(Book1.author, "Nuha Ali");
+  strcpy(Book1.subject, "C Programming Tutorial");
+  Book1.book_id = 6433;
+
+  strcpy(Book2.title, "C Programming2");
+  strcpy(Book2.author, "Nuha Ali2");
+  strcpy(Book2.subject, "C Programming Tutorial2");
+  Book2.book_id = 64339;
+
+  printBook(&Book1);
+  printBook(&Book2);
+
+  return 0;
+}
+
+void printBook(struct Books *book)
+{
+  printf("Book title: %s \n", book->title);
+  printf("Book author: %s \n", book->author);
+  printf("Book subject: %s \n", book->subject);
+  printf("Book book_id: %d \n", book->book_id);
+}
